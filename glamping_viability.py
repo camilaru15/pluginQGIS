607e5eed5bb4812c6ac7a2419d8e707b6b635f6a
camilaru15 <camilarueda715@gmail.com>
2026-04-27 19:42:40 +0700

    Puglin creado donde crea y realiza los calculos correspondientes para encontrar la viabilidad

--- glamping_viability.py
+++ glamping_viability.py
@@ -1,5 +1,6 @@
 from qgis.PyQt.QtWidgets import QAction, QMessageBox
 from qgis.core import *
+from PyQt5.QtCore import QVariant
 from qgis.gui import *
 import os.path
 
@@ -20,8 +21,49 @@ class GlampingViability:
         self.iface.removeToolBarIcon(self.action)
 
     def run(self):
-        QMessageBox.information(
-            self.iface.mainWindow(),
-            "Glamping Viability",
-            "¡Plugin funcionando correctamente!"
-        )
+        # Buscar la capa
+        layers = QgsProject.instance().mapLayersByName('Muestreado')
+        
+        if not layers:
+            QMessageBox.warning(None, "Error", "No se encontró la capa puntos_final")
+            return
+        
+        layer = layers[0]
+
+        # Crear campo viabilidad si no existe
+        if 'viabilidad' not in [f.name() for f in layer.fields()]:
+            layer.startEditing()
+            layer.addAttribute(QgsField('viabilidad', QVariant.Double))
+            layer.updateFields()
+
+        # Iniciar edición SI NO está activa
+        if not layer.isEditable():
+            layer.startEditing()
+
+        idx_viab = layer.fields().indexFromName('viabilidad')
+        
+        # Obtener índices de campos
+        idx_acces = layer.fields().indexFromName('acces_norm')
+        idx_turismo = layer.fields().indexFromName('turis_norm')
+        idx_pend = layer.fields().indexFromName('pend_norm')
+        idx_viab = layer.fields().indexFromName('viabilidad')
+        
+        # Comprobar campos
+        if -1 in (idx_acces, idx_turismo, idx_pend):
+            QMessageBox.warning(None, "Error", "Faltan campos necesarios")
+            return
+        
+        # Calcular viabilidad
+        for feat in layer.getFeatures():
+            acces = feat[idx_acces] if feat[idx_acces] is not None else 0
+            turismo = feat[idx_turismo] if feat[idx_turismo] is not None else 0
+            pendiente = feat[idx_pend] if feat[idx_pend] is not None else 0
+            
+            viabilidad = (0.3 * acces) + (0.3 * turismo) + (0.4 * pendiente)
+            
+            layer.changeAttributeValue(feat.id(), idx_viab, viabilidad)
+        
+        # Guardar cambios
+        layer.commitChanges()
+        
+        QMessageBox.information(None, "Éxito", "Viabilidad calculada correctamente")
